21ad6f05015f63837f56aaaa31e721d12d172856
Arya Emami <aryaemami59@yahoo.com>
2024-12-14 09:24:24 -0600

    Add minimal JSDocs to constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,19 +2,83 @@ import { homedir, tmpdir } from 'node:os';
 import * as path from 'node:path';
 import type { Options, ValidModes } from './types.js';
 
-const getHomeOrTmp = () => homedir() || tmpdir();
-const homeOrTmpDirectoryPath = /* @__PURE__ */ getHomeOrTmp();
+/**
+ * Returns the home directory path of the current user if available,
+ * otherwise returns the system's temporary directory path.
+ *
+ * @returns The path to the home directory or the temporary directory.
+ *
+ * @internal
+ * @since 3.0.0
+ */
+const getHomeOrTmpDirectoryPath = () => homedir() || tmpdir();
+
+/**
+ * The path to the home or temporary directory.
+ *
+ * @internal
+ * @since 3.0.0
+ */
+const homeOrTmpDirectoryPath = /* @__PURE__ */ getHomeOrTmpDirectoryPath();
+
+/**
+ * The name of the directory used for temporary storage.
+ *
+ * @internal
+ * @since 3.0.0
+ */
 export const stashDirectoryName = 'tmp';
+
+/**
+ * The name of the configuration file used by the Tiged tool.
+ * This file contains settings and options for the tool.
+ *
+ * @internal
+ * @since 3.0.0
+ */
 export const tigedConfigFileName = 'degit.json';
+
+/**
+ * The absolute path to the cache directory used by the `tiged`.
+ *
+ * @internal
+ * @since 3.0.0
+ */
 export const cacheDirectoryPath = /* @__PURE__ */ path.join(
   homeOrTmpDirectoryPath,
   '.degit',
 );
+
+/**
+ * A set of valid modes of operation for `tiged`.
+ *
+ * This set includes the following modes:
+ * - 'tar': Represents the tar mode.
+ * - 'git': Represents the git mode.
+ *
+ * @internal
+ * @since 3.0.0
+ */
 export const validModes = /* @__PURE__ */ new Set([
   'tar',
   'git',
 ] as const satisfies readonly ValidModes[]) satisfies ReadonlySet<ValidModes>;
+
+/**
+ * The name of the file where access logs are stored.
+ * This file is expected to be in JSON format.
+ *
+ * @internal
+ * @since 3.0.0
+ */
 export const accessLogsFileName = 'access.json';
+
+/**
+ * Default options used by `tiged`.
+ *
+ * @internal
+ * @since 3.0.0
+ */
 export const tigedDefaultOptions = {
   disableCache: false,
   force: false,
@@ -23,6 +87,14 @@ export const tigedDefaultOptions = {
   subgroup: false,
   verbose: false,
 } as const satisfies Options;
+
+/**
+ * A constant object that maps supported git hosting
+ * service names to their respective domain suffixes.
+ *
+ * @internal
+ * @since 3.0.0
+ */
 export const supportedHosts: Record<string, string> = {
   github: '.com',
   gitlab: '.com',
