216b1d6ed1c8c9d022f13c40f46e1f868d94e6c7
Arya Emami <aryaemami59@yahoo.com>
2024-12-16 18:53:04 -0600

    Create `cacheDirectoryPath` folder so `glob` does not fail

--- src/bin.ts
+++ src/bin.ts
@@ -105,6 +105,8 @@ async function main(): Promise<void> {
 
     const accessLookup = /* @__PURE__ */ new Map<string, number>();
 
+    await fs.mkdir(cacheDirectoryPath, { recursive: true });
+
     const accessJsonFiles = glob(`**/${accessLogsFileName}`, {
       cwd: cacheDirectoryPath,
     });
